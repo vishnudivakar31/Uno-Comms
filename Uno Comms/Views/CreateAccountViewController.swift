17079f39bf39a386850a5b5693e6423227052530
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-05-31 08:11:52 -0400

    upload profile picture action impl

--- Uno Comms/Views/CreateAccountViewController.swift
+++ Uno Comms/Views/CreateAccountViewController.swift
@@ -16,6 +16,7 @@ class CreateAccountViewController: UIViewController {
     @IBOutlet weak var confirmPasswordTextField: UITextField!
     @IBOutlet weak var submitButton: UIButton!
     
+    private let imagePicker = UIImagePickerController()
     private let authenticationService = AuthenticationService()
     
     
@@ -24,6 +25,7 @@ class CreateAccountViewController: UIViewController {
         perpareView()
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        imagePicker.delegate = self
     }
     
     @objc private func keyboardWillShow(notification: NSNotification) {
@@ -40,9 +42,17 @@ class CreateAccountViewController: UIViewController {
         }
     }
     
+    @IBAction func onUploadPictureTapped(_ sender: Any) {
+        getPicture()
+    }
+    
+    @IBAction func onSubmitTapped(_ sender: Any) {
+    }
+    
     private func perpareView() {
         changePlaceholderColor()
         beautifySubmitButton()
+        beautifyProfilePicture()
         assignKeyboardDelegatesToTextField()
         assignGestureToGoBack()
     }
@@ -67,6 +77,12 @@ class CreateAccountViewController: UIViewController {
         confirmPasswordTextField.attributedPlaceholder = NSAttributedString(string: "Confirm Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor.darkGray])
     }
     
+    private func beautifyProfilePicture() {
+        profileImageView.layer.cornerRadius = profileImageView.frame.size.width / 2
+        profileImageView.layer.borderWidth = 3.0
+        profileImageView.layer.borderColor = #colorLiteral(red: 0.07602740079, green: 0.1669456363, blue: 0.3494701087, alpha: 1)
+    }
+    
     private func beautifySubmitButton() {
         submitButton.backgroundColor = .systemBlue
         submitButton.layer.cornerRadius = 5
@@ -79,20 +95,53 @@ class CreateAccountViewController: UIViewController {
         confirmPasswordTextField.delegate = self
     }
     
-    @IBAction func onUploadPictureTapped(_ sender: Any) {
+    private func getPicture() {
+        let alertController = UIAlertController(title: "Select Media", message: "You can select a photo from your photos or you can snap a photo now.", preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { alertAction in
+            self.pickImage(source: "camera")
+        }
+        
+        let photoAction = UIAlertAction(title: "Photos", style: .default) { alertAction in
+            self.pickImage(source: "photos")
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(cameraAction)
+        alertController.addAction(photoAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
     
-    @IBAction func onSubmitTapped(_ sender: Any) {
+    private func pickImage(source: String) {
+        imagePicker.allowsEditing = true
+        if source == "camera" {
+            imagePicker.sourceType = .camera
+        } else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        present(imagePicker, animated: true, completion: nil)
     }
-    
-    
     
 }
 
-// MARK:- Extenstion for TextField Delegate
+// MARK:- Extension for TextField Delegate
 extension CreateAccountViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+// MARK:- Extension for ImagePicker
+extension CreateAccountViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else {
+            fatalError("Expected an image, but was provided with \(info)")
+        }
+        profileImageView.image = image
+        picker.dismiss(animated: true, completion: nil)
     }
 }
