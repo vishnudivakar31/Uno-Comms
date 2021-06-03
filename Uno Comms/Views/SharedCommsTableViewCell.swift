2e856cee4e28897d2323c73be99c875e7bfe9e30
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-06-03 10:13:57 -0400

    comms shared delegate

--- Uno Comms/Views/SharedCommsTableViewCell.swift
+++ Uno Comms/Views/SharedCommsTableViewCell.swift
@@ -7,14 +7,50 @@
 
 import UIKit
 
+protocol SharedCommsDelegate {
+    func onSharedCallback(documentID: String?)
+    func onDeleteCallback(documentID: String?)
+}
+
 class SharedCommsTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var identifierLabel: UILabel!
+    @IBOutlet weak var commLogo: UIImageView!
+    @IBOutlet weak var commIdentifier: UILabel!
+    
+    public var identifierLabelText: String?
+    public var commLogoImage: UIImage?
+    public var commIdentifierText: String?
+    public var documentID: String?
+    public var sharedCommsDelegate: SharedCommsDelegate?
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        if let identifierLabelText = identifierLabelText {
+            identifierLabel.text = identifierLabelText
+        }
+        
+        if let commLogoImage = commLogoImage {
+            commLogo.image = commLogoImage
+        }
+        
+        if let commIdentifierText = commIdentifierText {
+            commIdentifier.text = commIdentifierText
+        }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    @IBAction func onSharedButtonTapped(_ sender: Any) {
+        sharedCommsDelegate?.onSharedCallback(documentID: documentID)
+    }
+    
+    @IBAction func onDeleteButtonTapped(_ sender: Any) {
+        sharedCommsDelegate?.onDeleteCallback(documentID: documentID)
     }
     
 }
