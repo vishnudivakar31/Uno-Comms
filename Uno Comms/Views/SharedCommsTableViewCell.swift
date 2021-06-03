6973ab150b337babb9f39b309f4a41286d08c681
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-06-03 09:11:36 -0400

    shared commms table view cell

---
+++ Uno Comms/Views/SharedCommsTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  SharedCommsTableViewCell.swift
+//  Uno Comms
+//
+//  Created by Vishnu Divakar on 6/3/21.
+//
+
+import UIKit
+
+class SharedCommsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
