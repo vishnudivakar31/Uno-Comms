98acb5948e914d955ebf6f08c58aaa3142bdb586
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-06-08 12:10:29 -0400

    orientation fix

--- Uno Comms/AppDelegate.swift
+++ Uno Comms/AppDelegate.swift
@@ -12,12 +12,16 @@ import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    let orientationLock: UIInterfaceOrientationMask = .portrait
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         return true
+    }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return orientationLock
     }
 
     // MARK: UISceneSession Lifecycle
